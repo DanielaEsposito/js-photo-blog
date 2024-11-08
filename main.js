fa007d57ef28898badb31b380c0cd05c034a9482
DanielaEsposito <esposito.daniela9817@gmail.com>
2024-11-08 11:08:26 +0100

    add img overlay

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ fetch(`https://jsonplaceholder.typicode.com/photos?_limit=6"`)
         <div class="col-12 col-md-6 col-lg-4  p-3">
              <div class="single-card">
                  <div class="single-card-img">
-                    <img src="${card.url}" alt="">
+                    <img src="${card.url}" alt="" data-url="${card.url}" >
                  </div>
                  <div class="card-description">${card.title}</div>
              </div>
@@ -24,15 +24,26 @@ fetch(`https://jsonplaceholder.typicode.com/photos?_limit=6"`)
     const singleCard = document.querySelectorAll('#container-cards .single-card');
     const overlay = document.getElementById("overlay");
     const btnClose=document.getElementById("btn-close");
+    const overlayImg = document.getElementById("img-overlay")
     
     singleCard.forEach((card) => {
-    //console.log(card);
-    card.addEventListener("click", function() {
-    overlay.classList.remove("d-none")
+         //console.log(card);
+   
+    card.addEventListener("click", function () {
+         //console.log(this);
+    overlayImg.innerHTML=``;   
+    overlay.classList.remove("d-none");
+    const url = this.querySelector("img").getAttribute('data-url');
+    overlayImg.innerHTML +=`<img src="${url}" alt="">`;
+         //console.log(overlayImg);
+    
     })
     })
+
     btnClose.addEventListener("click",()=>{
         overlay.classList.add("d-none");
+        
+        
     })
 } )
 
